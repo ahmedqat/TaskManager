d240a8e8734157d12963a581d2095a597c678035
ahmedqat <ahmedlabeedqat@gmail.com>
2023-09-27 10:58:00 +0800

    Code Done

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -3,13 +3,25 @@ package com.example.taskmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +34,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    TaskImage(completeMsg = stringResource(id = R.string.task_complete),
+                        praise = stringResource(id = R.string.praise_msg)
+                    )
 
                 }
             }
@@ -31,13 +46,43 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TaskText(completeMsg: String, praise: String, modifier: Modifier = Modifier){
-    
+   Column (horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+
+       Text(
+           text = completeMsg,
+           fontWeight = FontWeight.Bold,
+           modifier = Modifier
+               .padding(top = 24.dp, bottom = 8.dp)
+
+       )
+       Text(
+           text = praise,
+           fontSize = 16.sp,
+
+
+       )
+   }
+
+}
+
+
+@Composable
+fun TaskImage(completeMsg: String, praise: String, modifier: Modifier = Modifier){
+    val image = painterResource(id = R.drawable.ic_task_completed)
+    Column (horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center){
+        Image(painter = image, contentDescription = null )
+        TaskText(completeMsg = completeMsg, praise = praise)
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TaskManagerTheme {
+        TaskImage(completeMsg = stringResource(id = R.string.task_complete),
+            praise = stringResource(id = R.string.praise_msg)
+        )
 
     }
 }
